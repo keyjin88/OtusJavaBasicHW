47e4bfa53a9a3de696cd419859725ef368632194
AV <keyjin88@gmail.com>
2021-11-02 23:19:40 +0300

    Добавил еще немного Java 9 Style

--- HW08/src/ru/vavtech/service/QuestionService.java
+++ HW08/src/ru/vavtech/service/QuestionService.java
@@ -3,7 +3,6 @@ package ru.vavtech.service;
 import ru.vavtech.model.Answer;
 import ru.vavtech.model.Question;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,16 +15,15 @@ public class QuestionService {
      * @return список вопросов
      */
     public static List<Question> getQuestionsList() {
-        List<Question> questions = new ArrayList<>();
-        questions.add(new Question(
+        return List.of(
+                new Question(
                         "Сколько будет 1+1?",
                         List.of(new Answer("2.", true),
                                 new Answer("Смотря в какой системе счисления.", false),
                                 new Answer("1 - я люблю JavaScript!", false)
                         )
-                )
-        );
-        questions.add(new Question(
+                ),
+                new Question(
                         "Как зовут глухого кролика?",
                         List.of(
                                 new Answer("Банни.", false),
@@ -33,9 +31,8 @@ public class QuestionService {
                                 new Answer("Эй ты!", false),
                                 new Answer("КРООООЛИК!!!!", true)
                         )
-                )
-        );
-        questions.add(new Question(
+                ),
+                new Question(
                         "Windows must die?",
                         List.of(
                                 new Answer("Да.", false),
@@ -43,8 +40,6 @@ public class QuestionService {
                                 new Answer("Yes!", false),
                                 new Answer("Да но XP был неплох.", true)
                         )
-                )
-        );
-        return questions;
+                ));
     }
 }
